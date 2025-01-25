0cc9c279198b9d2ad2f87caf9b8d1e478125645f
lidaning <41282656+relidaning@users.noreply.github.com>
2025-01-26 02:41:07 +0800

    add comments

--- autofocus/content.js
+++ autofocus/content.js
@@ -1,8 +1,8 @@
 console.log('alt+tab to chrome.')
-var keywordElement = document.getElementById('kw')
-if(!keywordElement) keywordElement = document.getElementById('APjFqb')
-if(!keywordElement) keywordElement = document.getElementById('chat-input')
-if(!keywordElement) keywordElement = document.getElementById('prompt-textarea')
+var keywordElement = document.getElementById('kw')//baidu
+if(!keywordElement) keywordElement = document.getElementById('APjFqb')//google
+if(!keywordElement) keywordElement = document.getElementById('chat-input')//chatgpt
+if(!keywordElement) keywordElement = document.getElementById('prompt-textarea')//deepseek
 if(keywordElement){
   keywordElement.focus()
 }
