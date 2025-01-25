b4f0a26366b5a5316a361ebfbe6b1d1cfe18e931
lidaning <41282656+relidaning@users.noreply.github.com>
2025-01-26 02:39:35 +0800

    add chatgpt & deepseek

--- autofocus/content.js
+++ autofocus/content.js
@@ -1,6 +1,8 @@
 console.log('alt+tab to chrome.')
 var keywordElement = document.getElementById('kw')
 if(!keywordElement) keywordElement = document.getElementById('APjFqb')
+if(!keywordElement) keywordElement = document.getElementById('chat-input')
+if(!keywordElement) keywordElement = document.getElementById('prompt-textarea')
 if(keywordElement){
   keywordElement.focus()
 }
