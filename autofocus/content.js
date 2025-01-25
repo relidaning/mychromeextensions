26339d31a263f77659355c9610c4fbcd13821f2a
lidaning <41282656+relidaning@users.noreply.github.com>
2025-01-26 02:15:43 +0800

    init project

---
+++ autofocus/content.js
@@ -0,0 +1,6 @@
+console.log('alt+tab to chrome.')
+var keywordElement = document.getElementById('kw')
+if(!keywordElement) keywordElement = document.getElementById('APjFqb')
+if(keywordElement){
+  keywordElement.focus()
+}
